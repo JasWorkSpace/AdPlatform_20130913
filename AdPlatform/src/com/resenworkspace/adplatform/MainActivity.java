d83a752a397c4407920e50717d8b8c43a1607339
jaselisan <jas.sheng@joyplus.tv>
2013-09-13 15:19:32 +0800

    Build AdPlatform Prj
    Build AdPlatform Prj by Jas@20130913

---
+++ AdPlatform/src/com/resenworkspace/adplatform/MainActivity.java
@@ -0,0 +1,34 @@
+package com.resenworkspace.adplatform;
+
+import com.Jasworkspace.Jasworkspace.ResenWorkSpaceConfig;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		TextView workspace = (TextView) findViewById(R.id.MainactivityTextView);
+		workspace.setText(getConfig());
+	}
+    private String getConfig(){
+    	StringBuffer sb = new StringBuffer();
+    	ResenWorkSpaceConfig config = ResenWorkSpaceConfig.getInstance();
+    	sb.append(config.getWorkSpace()).append("\n")
+ 	   .append(config.getEmail()).append("\n")
+    	   .append(config.getWorkSpaceCompany()).append("\n");
+    	return sb.toString();
+    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
