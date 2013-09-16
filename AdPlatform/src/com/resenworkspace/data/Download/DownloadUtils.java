d6745f6d1a02fd246b390933334da041def775d8
jaselisan <jas.sheng@joyplus.tv>
2013-09-16 10:29:54 +0800

    Add adpub and file DB
    Add adpub and file DB

---
+++ AdPlatform/src/com/resenworkspace/data/Download/DownloadUtils.java
@@ -0,0 +1,17 @@
+package com.resenworkspace.data.Download;
+
+public class DownloadUtils {
+
+	public static final String DOWNLOAD_FINISH  = "finish";
+    public static final String DOWNLOAD_LOADING = "loading";
+    public static final String DOWNLOAD_WAITING = "waiting";
+    
+    public static boolean CheckDownloadStateAviable(String state){
+    	if(state == null)return false;
+    	if(DOWNLOAD_FINISH.equals(state)
+    			|| DOWNLOAD_LOADING.equals(state)
+    			|| DOWNLOAD_WAITING.equals(state))
+    		return true;
+    	return false;
+    }
+}
